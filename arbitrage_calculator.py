2ff7407cd6560d96c5faf6f0f0b33ab3e3f81324
P-x-J <pablojosemunozaranda@gmail.com>
2025-06-24 13:56:19 +0000

    Added DataMarket class to arbitrage_calculator.py

--- arbitrage_calculator.py
+++ arbitrage_calculator.py
@@ -2,10 +2,11 @@ import sys
 import requests
 import webbrowser
 import logging
+import json
 
 
-logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w"
-                    format="%(acstime)s - %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w",
+                    format="%(asctime)s - %(levelname)s - %(message)s")
 
 logger = logging.getLogger(__name__)
 # Build a function that takes operation's values and determines whether there's an arbitrage opportunity withing these values.
@@ -33,7 +34,7 @@ class MarketsData():
     def __init__(self, gamma_api_data_url: str):
         self.gamma_api_data_url = gamma_api_data_url
     
-    def get_markets(self) -> list:
+    def get_markets(self) -> list[dict[str]]:
         # Export active markets in polymarkets data
 
         # Use querystrings to list the market with various filtering and sorting options.
@@ -61,42 +62,72 @@ class MarketsData():
         return decoded_markets
     
 
-    def get_events(self) -> list:
+    def get_events(self) -> list[dict[str, any]]:
         response = requests.request("GET", self.gamma_api_data_url, params=self.querystring)
         response = response.text
         response_json = json.loads(response)
 
-        decoded_market = []
+        decoded_event_markets = []
 
         for event in response_json:
             # get the list of multi-markets events of the recent events
+            if len(event["markets"])>= 1:
 
+                id = event.get("id")
+                event_slug = event.get("slug")
+                for tag in event.get("tags"):
+                    tid = tag.get("id")
 
+                decoded_event_markets.append({"id": id, "tid": tid, "slug": slug, "markets": []})
 
+                for market in event.get("markets"):
+                    outcome_price = market.get("outcomePrices")
+                    if outcome_price == None:
+                        pass
+                    else:
+                        id = market.get("id")
+                        slug = market.get("slug")
+                        markets = decoded_event_markets["markets"]
+                        markets.append({"id": id, "outcomePrices": outcome_price, "slug": slug})
+            else:
+                pass
+        return decoded_event_markets
 
-def calc_arb_percent(outcome_A_decimal, outcome_B_decimal):
+            
+                    
+
+class Probabilities():
+    """Manage the prices from the market's outcomes and determine whether an arbitrage opportunity exist"""
+    def __init__():
+        
+
+def calc_arb_percent(outcome_A_decimal, outcome_B_decimal) -> tuple[bool, str]:
     """Determines whether an arbitrage opportunity exists"""
     #Expects the decimal percentage of the two possible outcomes.
     try: 
-        float(outcome_A_decimal, outcome_B_decimal)
+        outcome_A_decimal = float(outcome_A_decimal)
+        outcome_B_decimal = float(outcome_B_decimal)
     except ValueError:
-        sys.exit("Outcome A & B weren't given as decimal numbers")
+        return False, "Invalid input: outcomes must be decimal numbers"
+    
+
+    # Calculate implied probabilites
+    player_A_prob = (1/outcome_A_decimal)*100
+    player_B_prob = (1/outcome_B_decimal)*100
+    total_prob = player_A_prob + player_B_prob
+
+
+    # If total_prob < 100% then there's an arbitrage opportunity
+    if total_prob < 100:
+        profit = 100 - total_prob
+        return True, f"+{profit:.2f}% arbitrage profit"
     else:
-        # Player A wins
-        player_A_wins = (1/outcome_A_decimal)x 100
-        player_B_wins = (1/outcome_B_decimal)x 100
-        total_prob = player_A_wins + player_B_wins
-        # If total_prob < 100% then there's an arbitrage opportunity
-        if total_prob < 100:
-            # Returns true if there's an arbitrage opportunity and profit percentage
-            return True, str("+", float(100-total_prob) + "%")
-        elif total_prob >= 100:
-            # returns false if there's no arbitrage opportunity and losing percentage
-            return True, str("-", float(-(100-total_prob)) + "%")
+        loss = total_prob - 100
+        return True, f"-{loss:.2f} inefficiency"
         
 
 
-def price_to_dec_odds(price_numbers):
+def price_to_dec_odds(price_numbers) -> list[int]:
     """Outputs the decimal odd number of the inputted outcome price"""
     decimal_odds_numbers = []
 
