4b5543b517c0899ac36f2bf31c411f709287e449
P-x-J <pablojosemunozaranda@gmail.com>
2025-06-25 13:48:22 +0000

    Added logging module

--- arbitrage_calculator.py
+++ arbitrage_calculator.py
@@ -33,7 +33,7 @@ class MarketsData():
     def __init__(self, gamma_api_data_url: str):
         self.gamma_api_data_url = gamma_api_data_url
     
-    def get_markets(self) -> list[dict[str]]:
+    def get_markets(self) -> list[dict[str, list[int]]]:
         # Export active markets in polymarkets data
 
         # Use querystrings to list the market with various filtering and sorting options.
@@ -46,17 +46,25 @@ class MarketsData():
         decoded_markets = []
  
         for market in response_json:
-            try:
-                outcome_price = market.get("outcomePrices")
-                if outcome_price == None:
-                    pass
-            except AttributeError:
-                # Only add the events that have outcomeprices listed
-                    pass
-            else:
+            outcome_prices_str = market.get("outcomePrices")
+            match = re.search(r'\[\"([0-9.]+)\", \"([0-9]+)\"\]', outcome_prices_str)
+            
+            if match:
+                logging.debug("Found outcomePrices")
+                outcome_prices = []
+                outcome_price1 = float(match.group(1))
+                outcome_price2 = float(match.group(2))
+                outcome_prices.append(outcome_price1)
+                outcome_prices.append(outcome_price2)
+
+
                 id = market.get("id")
                 slug = market.get("slug")
-                decoded_markets.append({"id": id, "outcomePrices": outcome_price, "slug": slug})
+                decoded_markets.append({"id": id, "outcomePrices": outcome_prices, "slug": slug})
+                logging.info("Append market to decoded_markets")
+            else:
+                logging.debug("Didn't find outcomePrices")
+                pass
 
         return decoded_markets
     
@@ -71,6 +79,7 @@ class MarketsData():
         for event in response_json:
             # get the list of multi-markets events of the recent events
             if len(event["markets"])>= 1:
+                logging.debug("Found an event with at least 1 market")
 
                 id = event.get("id")
                 event_slug = event.get("slug")
@@ -86,6 +95,7 @@ class MarketsData():
 
 
                     if match:
+                        logging.debug("Found outcomePrices")
                         outcome_prices = []
                         outcome_price1 = float(match.group(1))
                         outcome_price2 = float(match.group(2))
@@ -98,9 +108,11 @@ class MarketsData():
                         # Make a list of markets inside the events dictionnary
                         decoded_event_markets["markets"].append({"id": id, "outcomePrices": outcome_prices, "slug": slug})
                     else: 
+                        logging.debug("Didn't find outcomePrices")
                         pass
             
             else:
+                logging.debug("Event with no markets")
                 pass
 
         return decoded_event_markets
@@ -115,45 +127,58 @@ class Probabilities():
     def __init__(self, outcome_prices: list[int]):
         self.outcome_prices = outcome_prices
 
-    
-    def get_outcomes(self, outcome_prices) -> tuple[int, int]:
-        """Use regular expressions to extract the outcome prices from an string"""
+    def check_outcome_prices(self) -> bool:
+        """Check that there are only two prices"""
+        if len(self.outcome_prices) == 2:
+            logging.debug("outcome_prices is a two-elements list")
+            return True
+        else:
+            logging.debug("outcome_prices isn't a two-element list")
+            return False
 
 
-    def calc_arb_percent(self, outcome_A_decimal, outcome_B_decimal) -> tuple[bool, str]:
+    def calc_arb_percent(self) -> tuple[bool, str]:
         """Determines whether an arbitrage opportunity exists"""
 
         # Expects the decimal percentage of the two possible outcomes.
         try: 
-            outcome_A_decimal = float(outcome_A_decimal)
-            outcome_B_decimal = float(outcome_B_decimal)
+            outcome_A_decimal = float(self.outcome_prices[0])
+            outcome_B_decimal = float(self.outcome_prices[1])
         except ValueError:
+            logging.debug("The outcomes A & B aren't given as floats")
             return False, "Invalid input: outcomes must be decimal numbers"
+
     
 
         # Calculate implied probabilities
         player_A_prob = (1/outcome_A_decimal)*100
         player_B_prob = (1/outcome_B_decimal)*100
         total_prob = player_A_prob + player_B_prob
+        logging.info("Calculated the implied probabilities")
 
 
         # If total_prob < 100% then there's an arbitrage opportunity
         if total_prob < 100:
             profit = 100 - total_prob
+            logging.info(f"We're winning +{profit:.2f}")
             return True, f"+{profit:.2f}% arbitrage profit"
+
         else:
             loss = total_prob - 100
+            logging.info(f"We're lossing -{loss:.2f}")
             return True, f"-{loss:.2f} inefficiency"
-        
+            
 
 
-    def price_to_dec_odds(self, price_numbers) -> list[int]:
+
+    def price_to_dec_odds(self, price_numbers: list[float]) -> list[int]:
         """Outputs the decimal odd number of the inputted outcome price"""
         decimal_odds_numbers = []
 
         for price in price_numbers:
-            decimal_odd_number = 1/int(price)
+            decimal_odd_number = 1/float(price)
             decimal_odds_numbers.append(decimal_odd_number)
+        logging.info("Returned decimal odd numbers")
     
         # Supposed to return a two-elements list as it's a binary market, Test with len(decimal_odds_numbers) = 2
         return decimal_odds_numbers
