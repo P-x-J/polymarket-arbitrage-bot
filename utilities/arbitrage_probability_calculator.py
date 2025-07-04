9b376900f2737d2a3021f43ba70e88b4f7bcc301
Pablo José <pablojosemunozaranda@gmail.com>
2025-07-04 15:41:33 +0200

    Add arbitrage_probability_calculator.py for probability calculations

---
+++ utilities/arbitrage_probability_calculator.py
@@ -0,0 +1,12 @@
+import logging
+
+log5 = logging.getLogger(__name__)
+
+class ProbabilityCalculator():
+    def __init__(self, outcome_odds_decimals: list[float]):
+        self.outcome_odds_decimals = outcome_odds_decimals
+
+    def calculate_probability(self) -> float:
+        decimals = [(1/decimal)*100 for decimal in self.outcome_odds_decimals]
+        return sum(decimals)
+            
