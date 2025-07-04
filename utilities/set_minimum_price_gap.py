3ad308f6e8961ae8286a108c11285b485a010122
Pablo José <pablojosemunozaranda@gmail.com>
2025-07-04 15:19:16 +0200

    Added set_minimum_price_gap.py

---
+++ utilities/set_minimum_price_gap.py
@@ -0,0 +1,24 @@
+import logging
+
+
+log3 = logging.getLogger(__name__)
+
+
+def set_minimum_price_gap() -> float:
+    # A minimum of 1.5 is recommended
+    logging.info('Requested user to set a minimum price gap number')
+    while True:
+        try:
+            minimum_price_gap = input('Set minimum price gap number: ')
+            minimum_price_gap = float(minimum_price_gap)
+            if minimum_price_gap <= 0:
+                raise ValueError
+        except ValueError:
+            logging.error("The minimum price gap must be a float")
+            pass
+        else:
+            return minimum_price_gap
+    
+    
+set_minimum_price_gap()
+    
