275b16113ed977e84c8276b794aa24f45261a65f
Pablo José <pablojosemunozaranda@gmail.com>
2025-07-08 15:38:31 +0200

    Organised import statemens

--- utils/markets_getter.py
+++ utils/markets_getter.py
@@ -1,5 +1,7 @@
 # Creates and refreshes all active markets inside a list
+
 import logging
+
 from multi_markets_data_parser import MultiMarketsDataParser
 from markets_data_parser import MarketsDataParser
 
