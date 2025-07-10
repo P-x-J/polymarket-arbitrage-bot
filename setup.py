f83ca8e60a6bc8ba687f52fe1624dfa19002a35b
P-x-J <pablojosemunozaranda@gmail.com>
2025-07-10 15:51:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(name='gusml', version='1.0', packages=find_packages())
-
+setup(
+    name='polymarket-arbitrage-bot',
+    version='0.1.0',
+    description='Scans Polymarket for arbitrage opportunities in both single-market (binary) and multi-market (categorical) events',
+    author='P-x-J',
+    url='https://github.com/P-x-J/Polymarket-Arbitrage-Bot',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Or your license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
