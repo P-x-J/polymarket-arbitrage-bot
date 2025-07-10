8037ca5db8b35631ab43c9902f9e83c3999fba85
Pablo José <pablojosemunozaranda@gmail.com>
2025-07-10 17:15:51 +0200

    Uploaded project  to PyPI

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 
+this_directory =  Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 setup(
     name="polymarket-arbitrage-bot",
     version="0.1.0",
-    description="A bot to detect and execute arbitrage opportunities on Polymarket",
+    long_description=long_description,                                     
+    long_description_content_type="text/markdown",  
     url="https://github.com/P-x-J/Polymarket-Arbitrage-Bot",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},                        
+    packages=find_packages(),                     
     install_requires=[
         "requests", "web3", #etc
     ],
     python_requires=">=3.8",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
